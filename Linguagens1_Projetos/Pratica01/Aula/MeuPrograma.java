32460414b680e58fe701cfaaee4973285416af39
HENRIQUE SILVA GODOY <16.01147-3@maua.br>
2020-03-02 12:53:41 -0300

    Aula 02 java

---
+++ Linguagens1_Projetos/Pratica01/Aula/MeuPrograma.java
@@ -0,0 +1,11 @@
+class MeuPrograma {
+    public static void main (String [] args) {
+        // Mensagem de saida do sistema
+        System.out.println("Minha primeira aplicação de Java! ");
+        System.out.println("Ola mundo");
+        System.out.println("Estou programando em modo Conan!");
+
+    }
+
+}
+
